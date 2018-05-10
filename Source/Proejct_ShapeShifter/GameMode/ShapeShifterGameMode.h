f0c48124b8266e0644a7e0f4849586cade470c92
Khizer22 <n01051898@humbermail.ca>
2018-05-10 17:14:55 -0400

    Blank Gamemode and player character

---
+++ Source/Proejct_ShapeShifter/GameMode/ShapeShifterGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShapeShifterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROEJCT_SHAPESHIFTER_API AShapeShifterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
